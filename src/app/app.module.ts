ea0e9ed69d5836b4fc9bdcbcfa5ac3ea81a0ce94
Pavel Stoyanov <p.stoyanov@us.411locals.com>
2018-11-23 01:26:46 +0200

    main menu add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule
